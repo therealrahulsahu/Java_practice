c2fa23f29613c833cf8203257c4e22aed2bec4e9
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2020-06-23 02:29:15 +0530

    Save practice files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class HelloWorld {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws ParseException {
-        String S="31/12/1998";
-        System.out.println(S.substring(0, S.length()-1));
+        System.out.println(Integer.valueOf(5));
     }
 }
--- src/UploadToDB.java
+++ src/UploadToDB.java
@@ -4,29 +4,29 @@ import com.database.MySQLTool;
 
 import java.util.ArrayList;
 
-public class Solution {
+public class UploadToDB {
         public static void main(String[] args) {
 		long csv_start_time = System.nanoTime();
-        CSVReader file = new CSVReader("src/DataUploading.csv");		// starting CSV Reader
-        ArrayList<CustomerInvoicePOJO> POJOS = file.get_ArrayList_of_POJO();	// getting POJO list from CSVReader object
+
+		// starting CSV Reader
+        CSVReader file = new CSVReader("src/DataUploading.csv");
+
+        // getting POJO list from CSVReader object
+        ArrayList<CustomerInvoicePOJO> POJOS = file.get_ArrayList_of_POJO();
 
 		System.out.println("CSV Read Time(ms): "+ ((System.nanoTime()-csv_start_time)/1000000));
 
-		// Initialising MySQL Class with url, username and password
+
 		long db_upload_start_time = System.nanoTime();
 
+		// Initialising MySQL Class with url, username and password
 		MySQLTool DB = new MySQLTool("jdbc:mysql://localhost:3306/project", "root", "root");
 
-		/*ArrayList<String> queries = new ArrayList<>();		// A ArrayList to store queries
-		for (CustomerInvoicePOJO x:POJOS) {
-			// appending query into queries
-			queries.add("insert into customer_invoice ("+x.comma_separated_keys()+") values ("+x.comma_separated_values()+");");
-		}
-		DB.insert_multiple_queries(queries); // Adding it to database
-		*/
-
+		// taking batch of "step" size
 		int step = 2000;
 		for (int i = 0; i < 50000; i+=step) {
+
+			// insert query is used to insert data
 			String query = "insert into customer_invoice (" + POJOS.get(0).comma_separated_keys() + ") values ";
 			try{
 				for (CustomerInvoicePOJO x:POJOS.subList(i, i+step)) {
